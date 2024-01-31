b1b3e2bc2a53cf1a0ab8e52aeaa0b933d9a0f411
Yağmur İnan <ygmrinan00@gmail.com>
2024-01-31 16:59:59 +0300

    main card component added

---
+++ src/pages/Home.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import MainCard from "../components/MainCard";
+import { db } from '../config/firebase';
+import { getDocs, collection } from 'firebase/firestore';
+
+const Home = () => {
+  const [recipeList, setRecipeList] = useState([]);
+
+  const recipeCollectionRef = collection(db, "yemek");
+
+  useEffect(() => {
+    const getRecipeList = async () => {
+      try {
+        const data = await getDocs(recipeCollectionRef);
+        const filteredData = data.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setRecipeList(filteredData);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    getRecipeList();
+  }, [recipeCollectionRef]);
+
+  return (
+    <div>
+      <h1>Lezzet Yolculuğu</h1>
+      {recipeList.map((yemek) => (
+        <MainCard
+          key={yemek.id}
+          name={yemek.name}
+          image={yemek.image}
+          description={yemek.description}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Home;
+
